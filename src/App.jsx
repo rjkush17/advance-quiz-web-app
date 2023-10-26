2f99f4172cdcb07aeeead6fb69122d590af9660f
rjkush17 <rishabhkushwahrj@gmail.com>
2023-10-26 14:16:51 +0530

    fixed deployment Error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Quizscreen from "./Components/quizscreen";
+import Quizscreen from "./Components/Quizscreen";
 
 function App() {
   const [screen, setScreen] = useState(true);
